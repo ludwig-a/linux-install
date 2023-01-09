4d2f6389e8bbbb867a6a9bcf6001fc2a21a814d0
Wolfgang <ludwig.ludwig.anderson@gmail.com>
2023-01-09 18:31:10 +0100

    test VM partition.sh - update 2

--- 10-before_reboot/10-partitions.sh
+++ 10-before_reboot/10-partitions.sh
@@ -1,47 +1,11 @@
 #!/bin/sh
 
-#echo "We are going to create a partition table in sda. Then, we will format partitions"
-#echo "We will create 3 partitions like this :"
-#echo "- sda1 for boot"
-#echo "- sda2 for swap"
-#echo "- sda3 for rest"
-#echo "Is it okay for you ? Write 1 for no and exit or 2 for yes :"
-
-#read answer
-
-#nb=0
-#while [ $nb -ne 1 ]; do
-#	case $distro in
-#		1) exit ;;
-#		2) nb=1 ;;
-#		*) echo "Write 1 for no and exit or 2 for yes !"
-#	esac
-#done
-
-
-
-#########################################################################################
-
 echo "We are going to create a partition table. Then, we will format partitions"
 echo "We will create 3 partitions ; for booting, for swap and for the rest"
 echo "Default partitions size : 1 = 500M, 2 = 2 * RAM = 17G, 3 = size of th rest"
-echo "Select the hard disk : 1 for sda, 2 for sdb and 3 to exit the script :"
 
-read hard_disk
-
-nb=0
-while [ $nb -ne 1 ]; do
-	case $hard_disk in
-		1) hard_disk="/dev/sda" && nb=1 ;;
-		2) hard_disk="/dev/sdb" && nb=1 ;;
-		3) exit ;;
-		*) echo "Select the hard disk : 1 for sda, 2 for sdb and 3 to exit the script :"
-	esac
-done
-
-#hard_disk1=$hard_disk"1"
-#hard_disk2=$hard_disk"2"
-#hard_disk3=$hard_disk"3"
+hard_disk="/dev/sda"
+#hard_disk="/dev/sdb"
 
 fdisk $hard_disk
 
@@ -84,11 +48,9 @@ EOF
 
 #########################################################################################
 
-# write the size of each partition as arguments
-
-
-
-#########################################################################################
+#hard_disk1=$hard_disk"1"
+#hard_disk2=$hard_disk"2"
+#hard_disk3=$hard_disk"3"
 
 #mkfs.fat -F32 $hard_disk1
 #mkswap $hard_disk2
