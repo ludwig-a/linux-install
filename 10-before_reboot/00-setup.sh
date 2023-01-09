d5ea4ca8f0c9925a0cc31fb7a4ff9c7352e18fad
Wolfgang <ludwig.ludwig.anderson@gmail.com>
2023-01-09 20:07:41 +0100

    test VM partition.sh - update 7

--- 10-before_reboot/00-setup.sh
+++ 10-before_reboot/00-setup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 echo "####################################################################"
-echo "Inside this folder, run all the script in order."
+echo "Inside this folder, run all the script in order.                   #"
 echo "####################################################################"
 echo
 echo "####################################################################"
 echo "STEP 1 : set up my hard drive."
-echo
 echo "For that, run the appropriate script with the right arguments number"
-echo "####################################################################"
+cd /root/linux-install/before_reboot/
 echo
 echo "####################################################################"
 echo "STEP 2 :"
