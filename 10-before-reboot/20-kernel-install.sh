f3f7381519e3ac3764db5fe797bd9767b3ced0f8
Wolfgang <ludwig.ludwig.anderson@gmail.com>
2023-01-11 17:25:05 +0100

    without 00-start.sh - update 2

--- 10-before-reboot/20-kernel-install.sh
+++ 10-before-reboot/20-kernel-install.sh
@@ -1,15 +1,14 @@
+
 #!/bin/sh
 
 pacman-key --refresh-keys
 pacstrap -K /mnt base linux linux-headers linux-firmware linux-atm
 arch-chroot /mnt
 
-cd
-pacman -Sy git --noconfirm
-git clone https://github.com/ludwig-a/linux-install
-cd linux-install/10-before-reboot
-./01-auto-install.sh || echo "problem to run 01-auto-install.sh"
-#loadkeys fr_CH-latin1
+#pacman -Sy git --noconfirm
+#git clone https://github.com/ludwig-a/linux-install
+#cd linux-install/10-before-reboot
+#./01-auto-install.sh || echo "problem to run 01-auto-install.sh"
 
 echo "###################################################"
 echo "Now run 30-basics_install.sh                      #"
